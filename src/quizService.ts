8050661bc59d83f71f2667cf7e057784a16083c2
Gabriel Hess <heg2@bfh.ch>
2025-10-13 09:59:47 +0200

    0.4.3 additional quiz cards

--- src/quizService.ts
+++ src/quizService.ts
@@ -1,34 +1,5 @@
 import { QuizCard } from "./models";
-
-const QUIZ: QuizCard[]  = [
-    {
-        id: '1',
-        question: 'Wie viel Energie verbraucht ein ChatGPT-Prompt im Durchschnitt?',
-        prompts: [
-            'Kurze Antwort: Wie viel Energie verbraucht ein ChatGPT-Prompt im Durchschnitt?',
-            'Ausführliche Antwort: Wie viel Energie verbraucht ein ChatGPT-Prompt im Durchschnitt?',
-            'Verbraucht ein ChatGPT-Prompt so viel Energie wie eine LED-Lampe in einer Sekunde, ein Smartphone in zehn Sekunden oder ein Laptop in einer Minute?'
-        ]
-    },
-    {
-        id: '2',
-        question: 'Wieviel CO₂ verursacht eine Google-Suche?',
-        prompts: [
-            'Wie viel CO2 entsteht bei einer durchschnittlichen Google-Suche?',
-            'Was erzeugt mehr CO2: Eine Frage durch Google zu beantworten, oder mit einem KI-Modell? Erstelle eine Tabelle mit den wichtigsten Einfluss-Faktoren.',
-            'Was erzeugt mehr CO2: Eine Frage durch Google zu beantworten, oder mit einem KI-Modell? Halte die Antwort kurz.'
-        ]
-    },
-    {
-        id: '3',
-        question: 'Wie wirkt sich die steigende Nutzung energieintensiver digitaler Technologien langfristig auf die Gesundheit aus?',
-        prompts: [
-            'Wie wirkt sich die steigende Nutzung energieintensiver digitaler Technologien langfristig auf die Gesundheit aus?',
-            'Erläutere, inwiefern sich die steigende Nutzung energieintensiver digitaler Technologien über längere Frist auf die Gesundheit auswirkt.',
-            'Mache eine Auflistung von Auswirkungen energieintensiver Technologien auf die Gesundheit.'
-        ]
-    }
-];
+import { QUIZ } from "./assets/quizcards";
 
 export default class QuizService {
     static lastQuizCard = -1;
