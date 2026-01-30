45c9b6e8fbe9a8068dae6eaba11a9b80390d4430
Gabriel Hess <heg2@bfh.ch>
2026-01-30 14:36:48 +0100

    go back

---
+++ env.ts
@@ -0,0 +1,21 @@
+export const ENV = {
+//                 // BeeChat & enerki                            
+//     BASE_URL:   'https://inference.mlmp.ti.bfh.ch',   
+//     ENDPOINT:   '/api/chat/completions',         
+//     MODEL:      'enerki', // use special BeeChat with system prompt                 
+//     TOKEN:      ''        // insert your TOKEN here
+// }
+
+//                 // LM Studio
+//     BASE_URL:   'http://localhost:1234',
+//     ENDPOINT:   '/v1/chat/completions',        
+//     MODEL:      'gpt-oss:120b',
+//     TOKEN:      ''         // 'not_needed_for_localhost'
+// }
+
+                // ChatGPT
+    BASE_URL:   'https://api.openai.com',
+    ENDPOINT:   '/v1/chat/completions',        
+    MODEL:      'gpt-4.1', // 'gpt-3.5-turbo',
+    TOKEN:      ''         // insert token here
+}
